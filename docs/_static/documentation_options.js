812a2a8bc888826d1ef6c63aa434c270da2015d7
Anna Danese <anna.danese@helmholtz-muenchen.de>
2021-09-05 12:27:39 +0200

    revert previous version website

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1+55.g0b3d807',
+    VERSION: '0.3.1+47.gb297b30',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
